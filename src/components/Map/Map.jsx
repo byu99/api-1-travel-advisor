8453bf6a541af9e75e24ff0f77db60f3ac39cf56
Bobby Yu <90119496+byu99@users.noreply.github.com>
2023-07-26 17:44:43 -0700

    halfway there

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 
-const Map = () => {
+import useStyles from "./styles";
+
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px)");
+
     return (
-        <header>
-            <h1>Map</h1>
-        </header>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: "AIzaSyCI6RdySGIe1Ef6kpS55tlfObTOx0yBJoc",
+                }}
+                // defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
+                onChildClick={""}
+            ></GoogleMapReact>
+        </div>
     );
 };
 

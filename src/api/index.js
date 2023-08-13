73e3fe43d3cb2617227f68dbef79d2b8a7030815
Bobby Yu <90119496+byu99@users.noreply.github.com>
2023-08-13 16:03:46 -0700

    some progress

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const URL =
-    "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
         const {
             data: { data },
-        } = await axios.get(URL, {
-            params: {
-                bl_latitude: sw.lat,
-                tr_latitude: ne.lat,
-                bl_longitude: sw.lng,
-                tr_longitude: ne.lng,
-            },
-            headers: {
-                "X-RapidAPI-Key":
-                    "45a57c3366msh77d437793cf64fdp1abe00jsnfb62f1ad34b6",
-                "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-            },
-        });
+        } = await axios.get(
+            `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+            {
+                params: {
+                    bl_latitude: sw.lat,
+                    tr_latitude: ne.lat,
+                    bl_longitude: sw.lng,
+                    tr_longitude: ne.lng,
+                },
+                headers: {
+                    "X-RapidAPI-Key":
+                        "45a57c3366msh77d437793cf64fdp1abe00jsnfb62f1ad34b6",
+                    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+                },
+            }
+        );
 
         return data;
     } catch (error) {
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -13,10 +13,16 @@ import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
 import useStyles from "./styles";
 
-const List = ({ places, childClicked, isLoading }) => {
+const List = ({
+    places,
+    childClicked,
+    isLoading,
+    type,
+    setType,
+    rating,
+    setRating,
+}) => {
     const classes = useStyles();
-    const [type, setType] = useState("restaurants");
-    const [rating, setRating] = useState("");
     const [elRefs, setElRefs] = useState([]);
 
     useEffect(() => {
@@ -77,7 +83,7 @@ const List = ({ places, childClicked, isLoading }) => {
                                 <PlaceDetails
                                     place={place}
                                     selected={Number(childClicked) === i}
-                                    refProp={elRefs[i]}
+                                    ref={elRefs[i]}
                                 />
                             </Grid>
                         ))}
